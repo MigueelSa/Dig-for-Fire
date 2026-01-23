4ba69ea82e0b6c7a17697be2364e6ff248897473
Miguel Sá <1996.sa.miguel@gmail.com>
2026-01-23 14:35:52 +0100

    feat: add parent consideration to album evaluating in recommend/recommend.py
    - include parent_space_matrix and taste_pv
    - update _similarity_scores to combine genre, parent, and tag weights
    - fix _library_genres_tags and _space_matrix to handle parent genres
    - prevent duplicate albums in _fetch_albums using canonical IDs
    - clean up _genre_tag_randomizer and _taste_vectors

--- recommender/recommend.py
+++ recommender/recommend.py
@@ -18,13 +18,14 @@ from utils.paths import output_path
 class Recommender:
 
     def __init__(self, library_path: str, email: str, app_name: str = "Dig-for-Fire", app_version: str = "0.1", k: int = 2, limit: int = 1, threshold: float = 0.3):
-        self.mb_library                                                                 =   MusicBrainz(app_name, app_version, email)
-        self.library                                                                    =   self._load_library(library_path)
-        self.genre_space_matrix, self.tag_space_matrix                                  =   self._space_matrix(self.library)
-        self.taste_gv, self.taste_tv                                                    =   self._taste_vectors()
-        self.app_name, self.app_version, self.email, self.k, self.limit, self.threshold =   app_name, app_version, email, k, limit, threshold   
-        self.used_tokens                                                                =   set()
-        self.excluded_albums                                                            =   self._excluded_albums()
+        self.mb_library                                                                     =   MusicBrainz(app_name, app_version, email)
+        self.library                                                                        =   self._load_library(library_path)
+        self.genre_space_matrix, self.parent_space_matrix, self.tag_space_matrix            =   self._space_matrix(self.library)
+        self.taste_gv, self.taste_pv, self.taste_tv                                         =   self._taste_vectors()
+        self.app_name, self.app_version, self.email, self.k, self.limit, self.threshold     =   app_name, app_version, email, k, limit, threshold   
+        self.used_tokens                                                                    =   set()
+        self.excluded_albums                                                                =   self._excluded_albums()
+        self.children_genres, self.parent_genres, self.parent_unseen_children, self.tags    =   self._pools()
 
     def _fetch_recommendations(self) -> LibraryData:
         k, limit, threshold = self.k, self.limit, self.threshold
@@ -35,9 +36,11 @@ class Recommender:
                 fetched_albums = self._fetch_albums(random_tokens, limit)
                 if not fetched_albums:
                     continue
-                fetched_albums_gspace_matrix, fetched_albums_tspace_matrix = self._space_matrix(fetched_albums, create_space=False)
-                genre_similarity_projections, tag_similarity_projections = cosine_similarity(fetched_albums_gspace_matrix, self.taste_gv.reshape(1, -1)), cosine_similarity(fetched_albums_tspace_matrix, self.taste_tv.reshape(1, -1))
-                similarity_scores = self._similarity_scores(genre_similarity_projections, tag_similarity_projections)
+                fetched_albums_gspace_matrix, fetched_albums_pspace_matrix, fetched_albums_tspace_matrix = self._space_matrix(fetched_albums, create_space=False)
+                genre_similarity_projections = cosine_similarity(fetched_albums_gspace_matrix, self.taste_gv)
+                parent_similarity_projections = cosine_similarity(fetched_albums_pspace_matrix, self.taste_pv)
+                tag_similarity_projections = cosine_similarity(fetched_albums_tspace_matrix, self.taste_tv)
+                similarity_scores = self._similarity_scores(genre_similarity_projections, parent_similarity_projections, tag_similarity_projections)
                 for album, similarity_score in zip(fetched_albums, similarity_scores.flatten()):
                     album_id = self.mb_library._canonical_album(album)
                     if similarity_score >= threshold and album_id not in kept_ids and album_id not in self.excluded_albums:
@@ -63,10 +66,12 @@ class Recommender:
                 top_albums.append(album_dict)
         return top_albums[:k]
 
-    def _similarity_scores(self, genre_similarity_projections: np.ndarray, tag_similarity_projections: np.ndarray, genre_weight: float = 1.0, tag_weight: float = 0.1) -> np.ndarray:
+    def _similarity_scores(self, genre_similarity_projections: np.ndarray, parent_similarity_projections: np.ndarray, tag_similarity_projections: np.ndarray, genre_weight: float = 0.65, parent_weight: float = 0.25) -> np.ndarray:
+        tag_weight = 1.0 - genre_weight - parent_weight
         genre_scores = genre_similarity_projections.flatten()
+        parent_scores = parent_similarity_projections.flatten()
         tag_scores = tag_similarity_projections.flatten()
-        combined_scores = (genre_weight * genre_scores + tag_weight * tag_scores) / (genre_weight + tag_weight)
+        combined_scores = (genre_weight * genre_scores + parent_weight * parent_scores + tag_weight * tag_scores) / (genre_weight + parent_weight + tag_weight)
         return combined_scores
     
     def recommend(self) -> str:
@@ -94,28 +99,32 @@ class Recommender:
         filtered_genres = list(genre_set)
         return filtered_genres, tags
 
-    def _library_genres_tags(self, library: LibraryData) -> tuple[list[str], list[str]]:
-        library_genres, library_tags = [], []
+    def _library_genres_tags(self, library: LibraryData) -> tuple[list[str], list[str], list[str]]:
+        library_genres, parent_genres, library_tags = [], [], []
         for album in library:
             genres, tags = self._album_genres_tags(album)
             library_genres.append(" ".join(genres))
             library_tags.append(" ".join(tags))
-        return library_genres, library_tags
+
+            parents = self._albums_parents(album)
+            parent_genres.append(" ".join(parents))
+
+        return library_genres, parent_genres, library_tags
 
     def _space_matrix(self, library: LibraryData, create_space=True) -> tuple[csr_matrix, csr_matrix]:
-        library_genres, library_tags = self._library_genres_tags(library)
+        library_genres, parent_genres, library_tags = self._library_genres_tags(library)
         if create_space:
-            self.gvectorizer, self.tvectorizer = TfidfVectorizer(lowercase=False, token_pattern=r"[^ ]+"), TfidfVectorizer(lowercase=False, token_pattern=r"[^ ]+")
-            X, Y = self.gvectorizer.fit_transform(library_genres), self.tvectorizer.fit_transform(library_tags)
+            self.gvectorizer, self.pvectorizer, self.tvectorizer = TfidfVectorizer(lowercase=False, token_pattern=r"[^ ]+"), TfidfVectorizer(lowercase=False, token_pattern=r"[^ ]+"), TfidfVectorizer(lowercase=False, token_pattern=r"[^ ]+")
+            X, Y, Z = self.gvectorizer.fit_transform(library_genres), self.pvectorizer.fit_transform(parent_genres), self.tvectorizer.fit_transform(library_tags)
         else:
-            X, Y = self.gvectorizer.transform(library_genres), self.tvectorizer.transform(library_tags)
-        return X, Y
+            X, Y, Z = self.gvectorizer.transform(library_genres), self.pvectorizer.transform(parent_genres), self.tvectorizer.transform(library_tags)
+        return X, Y, Z
 
-    def _taste_vectors(self) -> tuple[np.ndarray, np.ndarray]:
-        gv, tv = self.genre_space_matrix.sum(axis=0), self.tag_space_matrix.sum(axis=0)
-        gv, tv = np.asarray(gv), np.asarray(tv)
-        gv, tv = normalize(gv), normalize(tv)
-        return gv, tv
+    def _taste_vectors(self) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        gv, pv, tv = self.genre_space_matrix.sum(axis=0), self.parent_space_matrix.sum(axis=0), self.tag_space_matrix.sum(axis=0)
+        gv, pv, tv = np.asarray(gv, dtype=float).reshape(1, -1), np.asarray(pv, dtype=float).reshape(1, -1), np.asarray(tv, dtype=float).reshape(1, -1)
+        gv, pv, tv = normalize(gv), normalize(pv), normalize(tv)
+        return gv, pv, tv
 
     def _pools(self) -> tuple[dict[str, int], dict[str, int], dict[str, list[str]], dict[str, int]]:
         all_genres, all_tags = [], []
@@ -127,15 +136,14 @@ class Recommender:
         children_genres, parent_genres = {}, {}
         parent_children = self.mb_library.tags._get_parents_children()
         for parent, children in parent_children.items():
-            if parent in genre_counts:
-                parent_genres[parent] = genre_counts[parent]
             for child in children:
                 children_genres[child] = genre_counts.get(child, 0)
+                parent_genres[parent] = parent_genres.get(parent, 0) + genre_counts.get(child, 0)
 
         parent_unseen_children = {
             parent: unseen
             for parent, children in parent_children.items()
-            if parent in genre_counts
+            if parent_genres.get(parent, 0) > 0
             and (unseen := [child for child in children if children_genres.get(child, 0) == 0])
         }
 
@@ -144,11 +152,25 @@ class Recommender:
         return children_genres, parent_genres, parent_unseen_children, tags
 
     def _genre_tag_randomizer(self, k: int, cg_probability: float = 0.6, pg_probability: float = 0.25) -> list[tuple[str, str]]:
-        children_genres, parent_genres, parent_unseen_children, tags = self._pools()
+        children_genres, parent_genres, parent_unseen_children, tags = self.children_genres, self.parent_genres, self.parent_unseen_children, self.tags
 
-        cg_items, cg_weights = list(children_genres.keys()), list(children_genres.values())
-        pg_items, pg_weights = [parent for parent in parent_genres.keys() if parent_unseen_children[parent]], list(parent_genres.values())
-        t_items, t_weights = list(tags.keys()), list(tags.values())
+        cg_items, cg_weights = [], []
+        for child, weight in children_genres.items():
+            if child not in self.used_tokens:
+                cg_items.append(child)
+                cg_weights.append(weight)
+
+        pg_items, pg_weights = [], []
+        for parent, weight in parent_genres.items():
+            if parent_unseen_children.get(parent) and parent not in self.used_tokens:
+                pg_items.append(parent)
+                pg_weights.append(weight)
+
+        t_items, t_weights = [], []
+        for tag, weight in tags.items():
+            if tag not in self.used_tokens:
+                t_items.append(tag)
+                t_weights.append(weight)
 
         random_tokens, tokens_set = [], set()
         while len(random_tokens) < k:
@@ -176,24 +198,31 @@ class Recommender:
         
 
     def _fetch_albums(self, tokens: list[tuple[str, str]], limit: int) -> LibraryData:
-        fetched_albums = []
+        fetched_albums = {}
         for token, _ in tokens:
-            result = mb.search_releases(query=f'tag:{token} AND primarytype:album', limit=limit)
-            releases = result.get("release-list", [])
+            try:
+                result = mb.search_releases(query=f'tag:{token} AND primarytype:album', limit=limit)
+                releases = result.get("release-list", [])
+            except (mb.ResponseError, mb.NetworkError) as e:
+                logging.warning(f"Skipping token {token}: {e}")
+                continue
+
             for release in releases:
                 release_id = release.get("id")
                 if not release_id:
                     continue
                 try:
                     full = mb.get_release_by_id(release_id, includes=["tags", "artist-credits", "release-groups", "media"])
-                except mb.ResponseError as e:
+                except (mb.ResponseError, mb.NetworkError) as e:
                     logging.warning(f"Skipping release {release_id}: {e}")
                     continue
+
                 fetched_album_dict = self.mb_library._feed_release(full)
                 fetched_album_id = self.mb_library._canonical_album(fetched_album_dict)
                 if fetched_album_id not in self.excluded_albums:
-                    fetched_albums.append(fetched_album_dict)
-        return fetched_albums
+                    fetched_albums[fetched_album_id] = fetched_album_dict
+
+        return list(fetched_albums.values())
     
     def _save_recommendations(self, recommendations: LibraryData, output_path = output_path("data")) -> None:
         recommendation_history_path = os.path.abspath(os.path.join(output_path, "recommendation-history-Dig-for-Fire.json"))
@@ -220,6 +249,15 @@ class Recommender:
             album_id = self.mb_library._canonical_album(album)
             excluded.add(album_id)
         return excluded
+
+    def _albums_parents(self, album: AlbumData) -> list[str]:
+        album_data = self.mb_library._feed_release(album)
+        genres, _ = self.mb_library.tags.genres_tags(album_data)
+        parents = set()
+        for genre in genres:
+            genre_parents = self.mb_library.tags.parents.get(genre) or []
+            parents.update(genre_parents)
+        return list(parents)
         
 
 
